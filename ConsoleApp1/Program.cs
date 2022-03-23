9aa22af768e5be322b008f56466b1dc8a8aa139d
Chappik <artemorel928@gmail.com>
2022-03-23 20:58:04 +0500

    Start

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 0, b = 0, vvod = 0;
+            while (true)
+            {
+                Console.Clear();
+                Console.WriteLine("Введите А\nВведите Б\nСложение\nВычитание\nУмножение\nДеление");
+                vvod = Convert.ToInt32(Console.ReadLine());
+
+
+
+
+
+
+            }
+        }
+}
